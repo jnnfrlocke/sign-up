e40804b0f89df9145dc652838b21d19ea12fe183
Jen <jnnfrlocke@gmail.com>
2017-04-29 19:41:10 -0500

    Change dots for each page

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,16 +1,22 @@
 $(".more").click(function() {
     $("#contactBtns").addClass("hide");
     $(".contactTxt").removeClass("hide");
+    $("#progCirc2").removeClass("fa-circle-o");
+    $("#progCirc2").addClass("fa-circle")
 })
 
 $("#meet").click(function() {
     $("#contactBtns").addClass("hide");
     $("#mtgInfo").removeClass("hide");
+    $("#progCirc2").removeClass("fa-circle-o");
+    $("#progCirc2").addClass("fa-circle")
 })
 
 $(".btnContinue").click(function() {
     $("#mtgInfo,.contactTxt").addClass("hide");
     $(".contactSubmit").removeClass("hide");
+    $("#progCirc3").removeClass("fa-circle-o");
+    $("#progCirc3").addClass("fa-circle")
 })
 
 $("#nameBtn").click(function() {
