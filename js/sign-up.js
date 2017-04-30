635ae332f18be0e7e17e54bc8ab732165f262473
Jen <jnnfrlocke@gmail.com>
2017-04-29 21:38:43 -0500

    Enable email field to pop up after user begins typing name.

--- js/sign-up.js
+++ js/sign-up.js
@@ -2,27 +2,29 @@ $(".more").click(function() {
     $("#contactBtns").addClass("hide");
     $(".contactTxt").removeClass("hide");
     $("#progCirc2").removeClass("fa-circle-o");
-    $("#progCirc2").addClass("fa-circle")
+    $("#progCirc2").addClass("fa-circle");
+    $(':input:enabled:visible:first').focus();
 })
 
 $("#meet").click(function() {
     $("#contactBtns").addClass("hide");
     $("#mtgInfo").removeClass("hide");
     $("#progCirc2").removeClass("fa-circle-o");
-    $("#progCirc2").addClass("fa-circle")
+    $("#progCirc2").addClass("fa-circle");
+    $(':input:enabled:visible:first').focus();
 })
 
 $(".btnContinue").click(function() {
     $("#mtgInfo,.contactTxt").addClass("hide");
     $(".contactSubmit").removeClass("hide");
     $("#progCirc3").removeClass("fa-circle-o");
-    $("#progCirc3").addClass("fa-circle")
+    $("#progCirc3").addClass("fa-circle");
+    $(':input:enabled:visible:first').focus();
 })
 
-$("#nameBtn").click(function() {
+$("#visitor").on("keyup", function(event) {
     $("#emailField").removeClass("hide");
-    $("#visitor,#nameBtn").addClass("hide");
-    var visitorName = $("#visitor").val();
-    $("p").append(visitorName + ", could I please have your email address?");
-
-})
+    $(':input:enabled:visible:first').focus();
+    var visitorName = event.target.value;
+    $("#autoName").text(visitorName);
+});
