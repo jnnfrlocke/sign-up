2e8a4d80dc25b6854dc15c10503d95396290dd43
Jen <jnnfrlocke@gmail.com>
2017-04-30 00:07:22 -0500

    Add comments for clarity

--- js/sign-up.js
+++ js/sign-up.js
@@ -1,19 +1,27 @@
+// About Sections
 $(".more").click(function() {
     $("#contactBtns").addClass("hide");
     $(".contactTxt").removeClass("hide");
+    // Progress Circles
     $("#progCirc2").removeClass("fa-circle-o");
     $("#progCirc2").addClass("fa-circle");
+    // Auto focus on first field
     $(':input:enabled:visible:first').focus();
 })
-$("#favTool").hide();
 
+// Meet Me Section
 $("#meet").click(function() {
     $("#contactBtns").addClass("hide");
     $("#mtgInfo").removeClass("hide");
+    // Progress Circles
     $("#progCirc2").removeClass("fa-circle-o");
     $("#progCirc2").addClass("fa-circle");
+    // Auto focus on first field
     $(':input:enabled:visible:first').focus();
 })
+
+// Web Meeting Tool Hide/Show
+$("#favTool").hide();
 
 $("select").change(function() {
     if ($("select").val() == "remote") {
@@ -23,17 +31,23 @@ $("select").change(function() {
     }
 })
 
+// Continue Button
 $(".btnContinue").click(function() {
     $("#mtgInfo,.contactTxt").addClass("hide");
     $(".contactSubmit").removeClass("hide");
+    // Progress Circles
     $("#progCirc3").removeClass("fa-circle-o");
     $("#progCirc3").addClass("fa-circle");
+    // Auto focus on first field
     $(':input:enabled:visible:first').focus();
 })
 
+// Show/Hide Email/Phone Section
 $("#visitor").on("keyup", function(event) {
     $("#emailField").removeClass("hide");
+    // Auto focus on first field
     $(':input:enabled:visible:first').focus();
+    // Dynamically add Name to text
     var visitorName = event.target.value;
     $("#autoName").text(visitorName);
 });
